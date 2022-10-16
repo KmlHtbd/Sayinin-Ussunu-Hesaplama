9791c68e5286efa538d2968eefc05de0ade2c9a4
kml htb <kmlhtbdev@gmail.com>
2022-10-16 15:10:11 +0300

    Initial commit

---
+++ src/sayiUssu.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sayiUssu {
+    public static void main(String[] args) {
+
+        int n,k;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayıyı giriniz:");
+        n = input.nextInt();
+        System.out.print("Üs olacak sayıyı giriniz:");
+        k = input.nextInt();
+
+        int i,total = 1;
+        for (i=1 ;i<=k ;i++){
+            total *= n;
+        };
+        System.out.println("Sonuç: "+total);
+    }
+}
